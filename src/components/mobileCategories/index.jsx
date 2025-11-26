29d5ff7531eac0337354d17eaa4ad7512eefe6be
moinkj <moheendealsarcade@gmail.com>
2025-11-26 19:31:34 +0500

    Update index.jsx

--- src/components/mobileCategories/index.jsx
+++ src/components/mobileCategories/index.jsx
@@ -10,6 +10,16 @@ const MobileCategories = ({ onClose, categories }) => {
     const [activeCategory, setActiveCategory] = useState(categories?.[0] || null);
 
     useEffect(() => {
+        const storedSlug = sessionStorage.getItem("selectedCategorySlug");
+        if (storedSlug) {
+            const foundCategory = categories.find(cat => cat.slug === storedSlug);
+            if (foundCategory) setActiveCategory(foundCategory);
+        } else {
+            setActiveCategory(null);
+        }
+    }, [categories]);
+
+    useEffect(() => {
         document.body.style.overflow = "hidden";
         return () => {
             document.body.style.overflow = "";
@@ -35,59 +45,70 @@ const MobileCategories = ({ onClose, categories }) => {
                     {/* Left Side Categories */}
                     <div className="left-side bg-[#f6f6f6] w-[36%] h-full overflow-y-scroll pb-12">
                         <ul className="mobile-cat-list text-[13px] font-semibold">
-
                             {categories?.length > 0 &&
                                 categories.map((cat) => (
                                     <li
-                                        key={cat.id}
-                                        onClick={() => { setActiveCategory(cat); sessionStorage.setItem("selectedCategorySlug", cat.slug) }}
-                                        className={`py-2 px-2 border-l-3 cursor-pointer ${activeCategory.slug === cat.slug
+                                        key={cat.slug}
+                                        onMouseEnter={() => setActiveCategory(cat)}
+
+                                        onClick={() => {
+                                            setActiveCategory(cat);
+                                            sessionStorage.setItem("selectedCategorySlug", cat.slug);
+
+                                        }}
+                                        className={`py-2 px-2 border-l-3 cursor-pointer ${activeCategory && activeCategory.slug === cat.slug
                                             ? "border-l-[#fb7701] bg-white text-black"
                                             : "border-l-transparent hover:border-l-[#fb7701] hover:bg-white text-[#555]"
                                             }`}
                                     >
                                         {cat.name}
                                     </li>
+
                                 ))}
                         </ul>
                     </div>
 
-                    {/* Right Side Subcategories */}
                     <div className="right-side w-full px-2 pt-2 overflow-y-scroll pb-30">
-                        <p className="text-[14px] font-semibold text-black">
-                            {activeCategory.name}
-                        </p>
-                        <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 pt-3">
+                        {activeCategory?.sub_categories?.length > 0 ? (
+                            <>
+                                <p className="text-[14px] font-semibold text-black">
+                                    {activeCategory.name}
+                                </p>
 
+                                <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 pt-3">
+                                    {activeCategory.sub_categories.map((sub) => (
+                                        <>
+                                            <Link
+                                                key={sub.slug}
+                                                href={`/c/${sub?.slug && sub.slug ? sub.slug : activeCategory?.slug}`}
+                                                onClick={() => {
+                                                    sessionStorage.setItem(
+                                                        "selectedCategorySlug",
+                                                        sub?.slug && sub.slug.trim() !== "" ? sub.slug : activeCategory?.slug
+                                                    );
+                                                    onClose();
+                                                }}
+                                                className="flex flex-col items-center text-center cursor-pointer group hover:scale-[1.06] transition-all duration-500 ease-in-out"
+                                            >
+                                                <Image
+                                                    src={sub.image || ProductBanner}
+                                                    alt={sub.name}
+                                                    width={80}
+                                                    height={80}
+                                                    className="w-[80px] h-[80px] rounded-full object-cover"
+                                                />
+                                                <p className="mt-2 text-[14px]">{sub.name}</p>
+                                            </Link>
+                                        </>
+                                    ))}
+                                </div>
 
-                            {activeCategory?.sub_categories?.length > 0 &&
-                                activeCategory.sub_categories.map((sub) => (
-                                 
-                                    <Link
-                                        key={sub.slug}
-                                        href={`/c/${sub?.slug && sub.slug ? sub.slug : activeCategory?.slug}`}
-                                   
-                                        onClick={() => {
-                                            sessionStorage.setItem(
-                                                "selectedCategorySlug",
-                                                sub?.slug && sub.slug.trim() !== "" ? sub.slug : activeCategory?.slug
-                                            );
-                                            onClose?.();
-                                        }}
-                                        className="flex flex-col items-center text-center cursor-pointer group hover:scale-[1.06] transition-all duration-500 ease-in-out"
-                                    >
-                                        <Image
-                                            src={sub.image || ProductBanner}
-                                            alt={sub.name}
-                                            width={80}
-                                            height={80}
-                                            className="w-[90%] h-auto rounded-full"
-                                        />
-                                        <p className="text-[13px] font-normal text-center mt-1">{sub.name}</p>
-                                    </Link>
-                                ))}
-                        </div>
+                            </>
+                        ) : (
+                            <p className="text-gray-500">No sub-categories available</p>
+                        )}
                     </div>
+
                 </div>
             </div>
         </>
